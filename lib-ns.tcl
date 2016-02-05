0bcc7b29cb504ef2352a91a47fe30502b37a4695
Marek Puzyniak <marek.puzyniak@tieto.com>
2016-02-05 14:46:02 +0100

    add exit status to ns_exec
    Signed-off-by: Marek Puzyniak <marek.puzyniak@tieto.com>

--- lib-ns.tcl
+++ lib-ns.tcl
@@ -44,22 +44,30 @@ proc ns_check_exist {name} {
 
 proc ns_open_shell {name} {
 	set ns_fd [open "| ip netns exec $name bash" r+]
-	fconfigure $ns_fd -blocking 0 -buffering line
+	fconfigure $ns_fd -blocking 0
 	return $ns_fd
 }
 
 
 proc ns_exec {ns_fd cmd} {
-	puts $ns_fd "$cmd\n"
+	puts $ns_fd "$cmd; echo ^^^$?^\n"
 	flush $ns_fd
-	puts "blocked [fblocked $ns_fd]"
-	puts "eof [eof $ns_fd]"
 	while {[set cmd_out [read $ns_fd]] == ""} {
-		puts "blocked [fblocked $ns_fd]"
-		puts "eof [eof $ns_fd]"
 		after 10
 	}
+	set start_idx 0
+	while {[set idx [string first "^" $cmd_out $start_idx]] >= 0} {
+		if { "^^^" == [string range $cmd_out $idx [expr $idx + 2]]} {
+			return [string range $cmd_out 0 [expr $idx - 2]]
+		}
+		set start_idx [expr $idx + 1]
+	}
 	return $cmd_out
+}
+
+proc ns_exec_nw {ns_fd cmd} {
+	puts $ns_fd "$cmd\n"
+	flush $ns_fd
 }
 
 ;# gets netns process list
